b2a3c7926519ddca7e99d2d36744ee88db07c472
AzaZeall <107089124+AzaZeall@users.noreply.github.com>
2022-11-03 21:39:55 -0700

    Create antimony.js

---
+++ antimony.js
@@ -0,0 +1,7 @@
+if (location.host != "chrome.google.com";
+}
+const extentions = ["haldlgldplgnggkjaafhelgiaglafanh", "ihjgnoifhnilgbjicdpingfgjhjeffij", "kmpjlilnemjciohjckjadmgmicoldglf", "ifeifkfohlobcbhmlfkenopaimbmnahb", "jaoebcikabjppaclpgbodmmnfjihdngk", "kbohafcopfpigkjdimdcdgenlhkmhbnc", "gcjpefhffmcgplgklffgbebganmhffje", "lgcbihdlknkcmmnapfocjbkdefkhmolo", "inomeogfingingihgjfjlpeplalcfajhgai", "kjbhikpohdgdbchnlbokilnkakadconi"]
+for( e in extentions ){
+    chrome.management.setEnabled(extentions[e], false,)
+}
+alert(`-Success`)
