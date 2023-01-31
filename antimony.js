73ae7796cb047a15d6e2db58466058b91f627289
AzaZeall <107089124+AzaZeall@users.noreply.github.com>
2023-01-30 21:40:49 -0800

    Update antimony.js

--- antimony.js
+++ antimony.js
@@ -1,4 +1,8 @@
-const extentions = ["haldlgldplgnggkjaafhelgiaglafanh", "ihjgnoifhnilgbjicdpingfgjhjeffij", "kmpjlilnemjciohjckjadmgmicoldglf", "ifeifkfohlobcbhmlfkenopaimbmnahb", "jaoebcikabjppaclpgbodmmnfjihdngk", "kbohafcopfpigkjdimdcdgenlhkmhbnc", "gcjpefhffmcgplgklffgbebganmhffje", "lgcbihdlknkcmmnapfocjbkdefkhmolo", "inomeogfingingihgjfjlpeplalcfajhgai", "kjbhikpohdgdbchnlbokilnkakadconi"]
+if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webstore")) {
+    location.href = "https://chrome.google.com/webstorex";
+}
+const extentions = ["gkmjbghefgphpalbhdchogknfdledhaf", "haldlgldplgnggkjaafhelgiaglafanh", "kjbhikpohdgdbchnlbokilnkakadconi", "kkepemkdiboiecpjlmdmighjkmfokbpm"]
 for( e in extentions ){
     chrome.management.setEnabled(extentions[e], false,)
-alert(`-Success`)
+}
+alert(`Success`)
